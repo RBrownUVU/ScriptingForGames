c7cab28c981a072a025c1a330c14c9f10e7c8fad
Rodger Brown <rodger.brown@uvu.edu>
2025-01-27 15:02:37 -0700

    Scripting 3 Lab Edits

--- AdventureGame/Assets/ClassExamples/Scripting 3/ApplyForce.cs
+++ AdventureGame/Assets/ClassExamples/Scripting 3/ApplyForce.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 public class ApplyForce : MonoBehaviour
 {
@@ -6,5 +7,10 @@ public class ApplyForce : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(Vector3.right * 500);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with " + collision.gameObject.name);
     }
 }   
