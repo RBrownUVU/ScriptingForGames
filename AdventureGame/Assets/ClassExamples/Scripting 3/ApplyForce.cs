c3be211818264fe99b439d45a832c2ec302c2932
Rodger Brown <rodger.brown@uvu.edu>
2025-01-27 17:49:05 -0700

    Added a couple of lines of code to ApplyForce.cs
    Added a couple of lines of code to ApplyForce.cs

--- AdventureGame/Assets/ClassExamples/Scripting 3/ApplyForce.cs
+++ AdventureGame/Assets/ClassExamples/Scripting 3/ApplyForce.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class ApplyForce : MonoBehaviour
 {
     private Rigidbody rb;
+    public int speed = 500;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.right * 500);
+        rb.AddForce(Vector3.right * speed);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision detected with " + collision.gameObject.name);
+        
     }
 }   
